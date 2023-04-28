183574ae12da2fd9ea4ab943df4779fc68e06ad7
ABiviano13 <abiviano0@outlook.com>
2023-04-28 18:19:56 +0200

    FEAT migration

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Project;
 use App\Models\Type;
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -19,6 +20,7 @@ class ProjectSeeder extends Seeder
     public function run(Faker $faker)
     {
         $type_ids = Type::all()->pluck('id')->all();
+        $technology_ids = Technology::all()->pluck('id')->all();
 
         for($i=0; $i < 100; $i++){
 
@@ -29,6 +31,10 @@ class ProjectSeeder extends Seeder
             $project->type_id = $faker->optional()->RandomElement($type_ids);
 
             $project->save();
+
+            //stiamo facendo la seguente operazione dopo che il project si salva poichè non ci sarebbe nessun id, altrimenti. 
+            
+            $project->technologies()->attach($faker->randomElements($technology_ids, rand(0, 5)));
 
         }
     }
